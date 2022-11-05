f3f47bcba907ac7e6b18d03432ce0f2017a18237
reyer3-bajoelarbol <92328462+reyer3-bajoelarbol@users.noreply.github.com>
2022-11-05 10:34:38 -0500

    Initial commit

---
+++ buildSrc/src/main/kotlin/BuildModules.kt
@@ -0,0 +1,6 @@
+object BuildModules {
+    const val shared = ":shared"
+    const val androidApp = ":androidApp"
+    const val iOSApp = ":iOSApp"
+    const val desktopApp = ":desktopApp"
+}
---
+++ shared/build.gradle.kts
@@ -0,0 +1,89 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    kotlin(Plugins.multiplatform)
+    id(Plugins.androidLibrary)
+    kotlin(Plugins.kotlinXSerialization) version Versions.kotlinSerialization
+    id(Plugins.sqlDelight) version Versions.sqlDelight
+    id(Plugins.nativeCoroutines)
+}
+
+android {
+    compileSdk = AndroidSdk.compileSdkVersion
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = AndroidSdk.minSdkVersion
+        targetSdk = AndroidSdk.targetSdkVersion
+    }
+}
+
+kotlin {
+    android()
+
+    val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
+        when {
+            System.getenv("SDK_NAME")?.startsWith("iphoneos") == true -> ::iosArm64
+            System.getenv("NATIVE_ARCH")?.startsWith("arm") == true -> ::iosSimulatorArm64
+            else -> ::iosX64
+        }
+    iosTarget("iOS") {}
+
+    jvm()
+
+    sourceSets {
+        sourceSets["commonMain"].dependencies {
+            api(MultiplatformDependencies.koinCore)
+
+            implementation(MultiplatformDependencies.kotlinxCoroutines)
+            implementation(MultiplatformDependencies.kotlinxSerialization)
+            implementation(MultiplatformDependencies.kotlinxDateTime)
+
+            implementation(MultiplatformDependencies.ktorCore)
+            implementation(MultiplatformDependencies.ktorSerialization)
+            implementation(MultiplatformDependencies.ktorLogging)
+
+            implementation(MultiplatformDependencies.sqlDelight)
+            implementation(MultiplatformDependencies.sqlDelightCoroutine)
+
+            implementation(MultiplatformDependencies.multiplatformSettings)
+            implementation(MultiplatformDependencies.multiplatformSettingsCoroutines)
+
+            api(MultiplatformDependencies.napier)
+        }
+
+        sourceSets["commonTest"].dependencies {
+            implementation(kotlin("test"))
+        }
+
+        sourceSets["androidMain"].dependencies {
+            implementation(MultiplatformDependencies.ktorAndroid)
+            implementation(MultiplatformDependencies.sqlDelightAndroid)
+        }
+
+        sourceSets["androidTest"].dependencies {
+        }
+
+        sourceSets["iOSMain"].dependencies {
+            implementation(MultiplatformDependencies.ktoriOS)
+            implementation(MultiplatformDependencies.sqlDelightNative)
+        }
+
+        sourceSets["iOSTest"].dependencies {
+        }
+
+        sourceSets["jvmMain"].dependencies {
+            api(MultiplatformDependencies.ktorJvm)
+            implementation(MultiplatformDependencies.sqlDelightJVM)
+        }
+
+        sourceSets["jvmTest"].dependencies {
+        }
+    }
+}
+
+sqldelight {
+    database(name = "AppDatabase") {
+        packageName = "com.vickikbt.kmptemplate.data.cache.sqldelight"
+        sourceFolders = listOf("kotlin")
+    }
+}
